3abebc6d35f6c21a623def43b1b9d73f59634e88
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-09-26 15:29:53 -0300

    Criando objeto literal e construtor

---
+++ ObjetoLiteralEConstrutor.js
@@ -0,0 +1,20 @@
+// Objeto literal
+
+const pessoa = {
+    nome: 'Ana Carolina',
+    sobrenome: 'Perdigão'  
+}
+
+console.log(pessoa);
+console.log(pessoa.nome);
+console.log(pessoa.sobrenome);
+
+//Objeto Construtor
+
+const pessoa2 = new Object();
+pessoa2.nome = 'Julia'
+pessoa2.sobrenome = 'Rabelo'
+
+console.log(pessoa2);
+console.log(pessoa2.nome);
+console.log(pessoa2.sobrenome);
