b1e8c1e2f93c44bc97f374a5cd4fce456e0098c3
anacperdigao <anacarolinaperdigao1@gmail.com>
2022-10-03 13:25:12 -0300

    Pegando as keys e values de objetos

---
+++ ObjectKeysEObjectValues.js
@@ -0,0 +1,19 @@
+// Object.keys - retorna as chaves
+
+const produto = {nome: "caneca", preco: 1.80};
+
+console.log(Object.keys(produto));
+
+
+// Object.values - retorna os valores
+
+const produto2 = {nome: "caneca", preco: 1.80};
+
+console.log(Object.values(produto2));
+
+
+// Object.entries - retorna as chaves e valores em arrays
+
+const produto3 = {nome: "caneca", preco: 1.80};
+
+console.log(Object.entries(produto3));
